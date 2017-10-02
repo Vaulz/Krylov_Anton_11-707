111c2cddcbd055d30ec65a1cea6a82d79baf6a4e
Anton <deagle7k@gmail.com>
2017-10-02 21:00:39 +0300

    part2_1 completed

---
+++ FALL2017/PS/PS1/part2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            int sum=0;
+            for (int i = 1; i < n; i++)
+                sum += i;
+            Console.WriteLine(sum);
+        }
+    }
+}
