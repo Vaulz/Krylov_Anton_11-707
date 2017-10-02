2a61db20d6d27a0c9a3b187e98bb761c047c1aec
Anton <deagle7k@gmail.com>
2017-10-02 18:24:53 +0300

    part1_4

---
+++ FALL2017/PS/PS1/part1_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            // k1 - Первая координата (где стоит)
+            string firstCoord = Console.ReadLine();
+            // k2 - Вторая кооридната (куда надо попасть)
+            string secondCoord = Console.ReadLine(); 
+            if (firstCoord[0] == secondCoord[0] || firstCoord[1] == secondCoord[1])
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+
+        }
+    }
+}
