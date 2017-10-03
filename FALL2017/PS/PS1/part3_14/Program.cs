72fe06f9d54389442b7b68f7d8e9fc9040ef4f71
Anton <deagle7k@gmail.com>
2017-10-03 20:49:29 +0300

    part3 completed

---
+++ FALL2017/PS/PS1/part3_14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace part3_14
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sequenceLength = 0, maxSequenceLength = 0;
+            int k=int.Parse(Console.ReadLine());
+            while (k!= 0)
+            {
+                if (k % 2 == 0)
+                    sequenceLength++;
+                else
+                {
+                    if (maxSequenceLength<sequenceLength)
+                        maxSequenceLength = sequenceLength;
+                    sequenceLength = 0;
+                }
+                k = int.Parse(Console.ReadLine());
+            }
+            if (maxSequenceLength < sequenceLength)
+                maxSequenceLength = sequenceLength;
+            Console.WriteLine(maxSequenceLength);
+        }
+    }
+}
---
+++ FALL2017/PS/PS1/part3_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace part3_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int previousNumber = int.Parse(Console.ReadLine());
+            int number;
+            string answer = "YES";
+            for (int i = 1; i < n; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number>previousNumber)
+                {
+                    answer = "NO";
+                    break;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
