ccaf16b1211c94cdb8cf29542c48d98976721b79
Anton <deagle7k@gmail.com>
2017-10-05 11:30:04 +0300

    Control Work 1

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            {
+                if ((x>=0 && x<=1 && y<=0 && y>=-1) || ((y>x)&&(x>=-1 && x<=0 && y>=-1 && y<=0))||((y>=0&&y<=1&&x>=-1&&x<0)&& y<=x*x))
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
