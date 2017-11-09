6a25190a35dc7c9805ec34f8d126c8cb9cbc3833
Anton <deagle7k@gmail.com>
2017-11-09 18:13:21 +0300

    part 1 completed

---
+++ FALL2017/PS/PS2/part1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace part1_2
+{
+    class Program
+    {
+
+        // Часть 1 №2
+        // Вычислить сумму ряда с заданной точностью и определить, на каком шаге начинает достигаться эта точность.
+        // e^(1/x)
+        // Крылов Антон 11-707
+
+        static void Main()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double eps = 0.0000000000000001;
+            double result = PowerOfExp(x, eps, out int iterations);
+            Console.WriteLine($"Результат вычислений:  {result}. \nЗаданная точность достигнута на {iterations} шаге.");
+        }
+        static double PowerOfExp(double x, double eps, out int iterations)
+        {
+            double item = 1,exp=1;
+            int k = 1;
+            while (item>eps)
+            {
+                item *= (2 * k * x + 1) / (x * x * (2 * k - 1) * 2 * k * (2*(k - 1) * x + 1));
+                exp += item;
+                k++;
+            }
+            iterations = k - 1;
+            return exp;
+        }
+    }
+}
---
+++ FALL2017/PS/PS2/part1_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace part1_8
+{
+    class Program
+    {
+
+        // Часть 1 №8
+        // Вычислить сумму ряда с заданной точностью и определить, на каком шаге начинает достигаться эта точность.
+        // sinh(x)
+        // Крылов Антон 11-707
+
+        static void Main()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double eps = 0.0000000000000001;
+            double result = PowerOfExp(x, eps, out int iterations);
+            Console.WriteLine($"Результат вычислений: \t{result}. \nТабличное значение: \t{Math.Sinh(x)}. \nЗаданная точность достигнута на {iterations} шаге.");
+        }
+        static double PowerOfExp(double x, double eps, out int iterations)
+        {
+            double item = x, sinh = x;
+            int k = 1;
+            while (item > eps)
+            {
+                item *=x*x/(2*k*(2*k+1));
+                sinh += item;
+                k++;
+            }
+            iterations = k - 1;
+            return sinh;
+        }
+    }
+}
