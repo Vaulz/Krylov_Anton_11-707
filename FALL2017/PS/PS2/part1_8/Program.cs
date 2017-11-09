6418f1a9853b56f69a8d983765a8fa7245b6d509
Anton <deagle7k@gmail.com>
2017-11-09 19:32:24 +0300

    Part 2 completed, the names of the methods are corrected

--- FALL2017/PS/PS2/part1_8/Program.cs
+++ FALL2017/PS/PS2/part1_8/Program.cs
@@ -6,7 +6,7 @@ namespace part1_8
     {
 
         // Часть 1 №8
-        // Вычислить сумму ряда с заданной точностью и определить, на каком шаге начинает достигаться эта точность.
+        // Вычислить сумму ряда с заданной точностью и определить, на каком шаге начинает достигаться эта точность
         // sinh(x)
         // Крылов Антон 11-707
 
@@ -14,10 +14,10 @@ namespace part1_8
         {
             double x = double.Parse(Console.ReadLine());
             double eps = 0.0000000000000001;
-            double result = PowerOfExp(x, eps, out int iterations);
+            double result = Sinh(x, eps, out int iterations);
             Console.WriteLine($"Результат вычислений: \t{result}. \nТабличное значение: \t{Math.Sinh(x)}. \nЗаданная точность достигнута на {iterations} шаге.");
         }
-        static double PowerOfExp(double x, double eps, out int iterations)
+        static double Sinh(double x, double eps, out int iterations)
         {
             double item = x, sinh = x;
             int k = 1;
---
+++ FALL2017/PS/PS2/part2_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace part2_6
+{
+    class Program
+    {
+
+        // Часть 2 №6
+        // Вычислить значение числа Пи через биномиальные коэффициенты №2 
+        // Крылов Антон 11-707
+
+        static void Main()
+        {
+            double eps = 0.0000000000000001;
+            double result = NumberPI(eps, out int iterations);
+            Console.WriteLine($"Результат вычислений: \t{result}. \nТабличное значение: \t{Math.PI}. \nЗаданная точность достигнута на {iterations} шаге.");
+        }
+        static double NumberPI(double eps, out int iterations)
+        {
+            double item = -6, pi = -6;
+            int k = 1;
+            while (Math.Abs(item) > eps)
+            {
+                item *=((50*k-6)*k*(2*k-1))/((50*k-56)*3*(3*k-1.0)*(3*k-2));
+                pi += item;
+                k++;
+
+            }
+            iterations = k;
+            return pi;
+        }
+    }
+}
